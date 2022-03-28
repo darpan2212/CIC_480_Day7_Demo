513eeb65279d2a36113172946e4cb1fcb64b7c46
Darpan Madlani <Darpan@proclink.com>
2022-03-28 11:43:53 +0530

    Array demo

---
+++ arrayDemo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+fruits[0]="Mangoes";
+fruits[1]="Oranges";
+fruits[2]="Apples";
+
+echo "All elements of array : "${fruits[@]};
+echo "Size of array : " ${#fruits[@]};
+
+echo "Printing index 1 element : " ${fruits[1]};
+
+echo "For loop array"
+for ((i=0;i<${#fruits[@]};i++))
+do
+	echo ${fruits[i]}
+done
